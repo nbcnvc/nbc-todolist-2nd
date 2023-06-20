3b9b3ad62fbcdc663c001d681dab1bb36e5235e2
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-20 16:22:41 +0900

    Add Header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,22 @@
+import { styled } from "styled-components";
+
+const HeaderWrapper = styled.div`
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border: 1px solid black;
+  padding: 0 10px;
+  margin: 20px 0;
+`
+
 const Header = () => {
   return (
-    <div>todo list v2</div>
+    <HeaderWrapper>
+      <span>Todo list v2</span>
+      <span>by React</span>
+    </HeaderWrapper>
   )
 }
 
