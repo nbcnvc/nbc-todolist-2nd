5344d7ba522009f417577617417fec9e90c98ce7
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-20 16:22:22 +0900

    Add container component for base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import { styled } from "styled-components";
+
 import Header from "./components/Header";
 import TodoDetail from "./components/TodoDetail/TodoDetail";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-import { createBrowserRouter } from "react-router-dom";
-
 export const router = createBrowserRouter([
   {
-    path: '/todos',
-    element: <App />
+    path: "/todos",
+    element: <App />,
   },
   {
-    path: '/todos/:todoId',
-    element: <TodoDetail />
-  }
-])
+    path: "/todos/:todoId",
+    element: <TodoDetail />,
+  },
+]);
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  min-width: 800px;
+
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <>
+    <Container>
       <Header />
       <TodoForm />
       <TodoList />
-    </>
+    </Container>
   );
 }
 
